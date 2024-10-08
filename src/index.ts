39c28dbe06b83a08c61643a8dc6c6dd4fa2fc739
Ingvar Vilkman <zeegin@zeegin.com>
2024-10-08 06:43:01 +0300

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export {
+  default as V8Proxy
+} from './core/V8Proxy';
+
+export {
+  default as useV8FormAttribute
+} from './useV8FormAttribute';
+
+export {
   default as V8WebAppProvider
 } from './V8WebAppProvider';
