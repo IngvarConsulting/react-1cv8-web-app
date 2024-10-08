0e4ae4618dd352eb62f572f267344f49056cca0e
Ingvar Vilkman <zeegin@zeegin.com>
2024-10-08 07:07:23 +0300

    fix module type and exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-export {
-  default as V8Proxy
-} from './core/V8Proxy';
+export { default as V8Proxy } from './core/V8Proxy';
 
-export {
-  default as useV8FormAttribute
-} from './useV8FormAttribute';
+export { default as useV8FormAttribute } from './useV8FormAttribute';
 
-export {
-  default as V8WebAppProvider
-} from './V8WebAppProvider';
+export { default as V8WebAppProvider } from './V8WebAppProvider';
